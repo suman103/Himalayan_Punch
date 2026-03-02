f174e9485ad4463090962841ada4e369c0d55371
suman103 <sumanmhrzn37@gmail.com>
2026-03-02 12:17:49 +0545

    Add place repository interface

---
+++ app/src/main/java/com/example/himalayanpunch/repository/PlaceRepo.kt
@@ -0,0 +1,5 @@
+package com.example.himalayanpunch.repository
+
+// PlaceRepo - interface for place/menu-item data access.
+// See BookingRepo.kt for the main interface definition.
+interface PlaceRepo : BookingRepo
